8e8c36c93f41208833ec9ede620f8f1634536cf9
tranthanhkhang2281999 <360tranthanhkhang2281999@gmail.com>
2020-06-10 21:30:51 +0700

    add change data button

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 import iris from './data.js';
 import { drawChart, getDataSetTwoCross } from './utility.js';
 
-const irisTwoCrossWithLabel = getDataSetTwoCross(iris, [3, 4]);
+const irisTwoCrossWithLabel = getDataSetTwoCross(iris, [1, 2]);
 let myCanvas = document.getElementById('my-canvas');
+let changeButton = document.querySelector('#change');
+
+let twoCross = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 3],
+  [2, 4],
+  [3, 4]
+];
 let context = myCanvas.getContext('2d');
 
 const allProps = {
@@ -16,4 +26,16 @@ const allProps = {
   MARKPOINT_FONT_FAMILY: 'Calibri'
 };
 
+let currentIndex = 1;
+
 drawChart({ ...allProps });
+
+changeButton.addEventListener('click', (e) => {
+  context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+
+  ++currentIndex > 6 ? (currentIndex = 1) : currentIndex;
+
+  allProps.irisTwoCross = getDataSetTwoCross(iris, twoCross[currentIndex]);
+
+  drawChart({ ...allProps });
+});
